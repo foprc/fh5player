b50addd37c52b60d5213c4f42a74718e6df073b9
foprctogo@gmail.com <foprctogo@gmail.com>
2014-06-22 19:03:26 +0800

    add events.

--- js/fh5player.js
+++ js/fh5player.js
@@ -17,27 +17,40 @@
         init: function(config){
             var me = this;
             var player = $("<audio id='fh5player' />");
-            $('body').append(player);
+            
             me.player = player;
             me.isPlaying = false;
+            me.timesnapInterval = null;
 
             //events
             me.ePause = 'PlayerPause';
             me.ePlay = 'PlayerPlay';
             me.eStop = 'PlayerStop';
+            me.eTimer = 'PlayerTimebar';
+
+            $('body').append(player);
 
             me.player.on('play', function(){
                 me.player.trigger(me.ePlay);
+                clearInterval(me.timesnapInterval);
+                var ProcessYet = 0;
+                var audio = me.player[0];
+                me.timesnapInterval = setInterval(function () {
+                    ProcessYet = (audio.currentTime / audio.duration)*100;
+                    me.player.trigger(me.eTimer, [ProcessYet]);
+                }, 1000);
             });
             me.player.on('pause', function(){
+                clearInterval(me.timesnapInterval);
+
                 if(me.player[0].ended){
                     me.player.trigger(me.eStop);
+                    me.player.trigger(me.eTimer, [100]);
                 }else{
                     me.player.trigger(me.ePause);
                 }
             });
 
-            me.setSound('test.mp3');
         },
         pause: function(){
             this.isPlaying = false;
@@ -47,13 +60,24 @@
             this.isPlaying = true;
             this.player[0].play();
         },
-        setSound: function(soundSrc){
-            this.player[0].src = soundSrc ? soundSrc : null;
-            if(soundSrc){
+        setSound: function(soundData){
+            this.player[0].src = soundData ? soundData.src : null;
+            if(soundData){
                 this.play();
             }
+        },
+        getPlayStatus: function(){
+            return this.isPlaying;
         }
     };
+
+    function timeDispose(number) {
+        var minute = parseInt(number / 60);
+        var second = parseInt(number % 60);
+        minute = minute >= 10 ? minute : "0" + minute;
+        second = second >= 10 ? second : "0" + second;
+         return minute + ":" + second;
+    }
 
     $.player = new fh5player();
  })($);
